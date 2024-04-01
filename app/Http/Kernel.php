3540a519bcbaacdb209e3654f4374d96d4128ddb
Gb Store <gb.bakeshop.app@gmail.com>
2024-04-02 06:39:58 +0800

    added new kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'throttle:150000000,1',
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
